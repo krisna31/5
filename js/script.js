c982d912e2301b4a68124665b3b054dfec50b82d
Krisna <krisnaaaputraaa@gmail.com>
2021-06-20 14:17:38 +0700

    berhasil nambah parallax di jumbo,about,and portd=folio

--- js/script.js
+++ js/script.js
@@ -1,18 +1,54 @@
-// jquery tolong carikan saya el pg-scrl kalo nemu
+// jquery tolong carikan saya el pg-scrl kalo nemu saat di-click jalankan fungsi dibawah ini
 $('.page-scroll').on('click', function (e) {
 
-    // ambil href
+    // ambil href dengan variabel tujuan
     var tujuan = $(this).attr('href');
-    //elemen yang bersangkutan
+    //ambil var href dengan var elTujuan
     var elemenTujuan = $(tujuan);
 
 
-    //move the scroll
+    // scroll di- animasikan dengan ambil dari jquerry easing website
     $('html,body').animate({
         scrollTop: elemenTujuan.offset().top - 50
     }, 888, 'easeInOutQuart');
 
-
+    // jadi saat href di click tidak akan ada respon nama nya e aja
     e.preventDefault();
+
+});
+
+// saat di load animasi jalan
+$(window).on('load', function () {
+    $('.pkiri').addClass('pmuncul')
+    $('.pkanan').addClass('pmuncul')
+})
+
+// scroll di px tertentu fungsi dijalankan
+$(window).scroll(function () {
+    var wscroll = $(this).scrollTop();
+
+    $('.jumbotron img').css({
+        'transform': 'translate(0px, ' + wscroll / 4 + '%)'
+    });
+
+    $('.jumbotron h1').css({
+        'transform': 'translate(0px, ' + wscroll / 2 + '%)'
+    });
+
+    $('.jumbotron p').css({
+        'transform': 'translate(0px, ' + wscroll / 1.2 + '%)'
+    });
+
+    // portfolio 
+    if (wscroll > $('.portfolio').offset().top - 244) {
+        $('.portfolio .thumbnail').each(function (i) {
+            setTimeout(function () {
+                $('.portfolio .thumbnail').eq(i).addClass('muncul');
+            }, 300 * (i + 1));
+
+        });
+
+    }
+
 
 });
